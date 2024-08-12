7c842d9673d4f6f7facd7af07d3d14016da95f6e
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-08-12 21:47:01 +0800

    WFTaskFactory::create_http_task的第四个参数使用回调函数

---
+++ httpTask2.cpp
@@ -0,0 +1,41 @@
+#include <workflow/WFFacilities.h>
+#include <workflow/WFTaskFactory.h>
+#include <signal.h>
+#include <iostream>
+using std::cout;
+
+static WFFacilities::WaitGroup waitGroup(1);
+
+void sighandler(int signum)
+{
+    cout << "done!\n";
+    waitGroup.done();
+}
+
+void httpCallback(WFHttpTask *httpTask)
+{
+    cout << "httpCallback is running!\n";
+    protocol::HttpResponse *resp = httpTask->get_resp();
+}
+
+int main()
+{
+    signal(SIGINT, sighandler);
+    
+    WFHttpTask *httpTask = WFTaskFactory::create_http_task(
+            "http://www.baidu.com", 
+            10, 10, 
+            httpCallback); //给任务对象申请内存并初始化
+    
+    protocol::HttpRequest *req = httpTask->get_req();  //找到请求的地址
+    
+    req->add_header_pair("Agent", "Workflow"); //给请求添加一个首部字段
+    req->add_header_pair("name", "Edward"); 
+    req->add_header_pair("method", "Callback"); 
+    
+    httpTask->start(); //异步启动任务,把任务交给框架,由框架调度资源运行
+    
+    waitGroup.wait();
+    cout << "finished!\n";
+    return 0;
+}
